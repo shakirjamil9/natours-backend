2d59b8a6c462f10b3a8cd05357c8d3428f5e93ac
shakirjamil9 <mianshakir9@hotmail.com>
2022-09-15 19:17:09 +0500

    return directly

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,11 +3,10 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
-const signToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+const signToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
-};
 
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
